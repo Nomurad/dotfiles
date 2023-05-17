51456a46710cfb06efe9af60c6a222733e059849
nomurad <nomurad@outlook.jp>
2023-05-18 01:52:31 +0900

    add jetpack install script

---
+++ vim_plugin_manager/install_jetpack_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -fLo ~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
+
---
+++ vim_plugin_manager/install_jetpack_win.ps1
@@ -0,0 +1,2 @@
+
+curl -fLo ~\vimfiles\pack\jetpack\opt\vim-jetpack\plugin\jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
